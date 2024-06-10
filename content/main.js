d4d23746ed008d340e0654366dc6a14d34733cbb
Hyperfied <hyperfied@outlook.com>
2024-06-10 19:47:57 +0100

    Added "pages"

--- content/main.js
+++ content/main.js
@@ -1,22 +1,31 @@
 let projectsButton = document.getElementById("projectsButton");
 let speedrunsButton = document.getElementById("speedrunsButton");
 
+let projectsContent = document.getElementById("projectsContent");
+let speedrunsContent = document.getElementById("speedrunsContent");
+
 let selectedButton = projectsButton;
-selectedButton.classList.add("selectedNav");
+let selectedContent = projectsContent;
 
 
 projectsButton.onclick = function() {
     selectedButton.classList.remove("selectedNav");
-
-    console.log("projects");
+    selectedContent.classList.add("hide");
 
     selectedButton = projectsButton;
+    selectedContent = projectsContent;
+    
     selectedButton.classList.add("selectedNav");
+    selectedContent.classList.remove("hide");
 }
 
 speedrunsButton.onclick = function() {
     selectedButton.classList.remove("selectedNav");
+    selectedContent.classList.add("hide");
 
     selectedButton = speedrunsButton;
+    selectedContent = speedrunsContent;
+
     selectedButton.classList.add("selectedNav");
+    selectedContent.classList.remove("hide");
 }
